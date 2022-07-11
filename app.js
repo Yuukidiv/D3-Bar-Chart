910797434cebc360e9d31cb520b8d4a57b582c82
Yuukidiv <67429515+Yuukidiv@users.noreply.github.com>
2022-07-11 15:16:51 +0200

    Remade project in React and D3

--- app.js
+++ app.js
@@ -1,20 +1,9 @@
-const dataset = [
-    [32, 322],
-    [23, 190]
-];
+// Per fare progetto: conoscere come tirare fuori data da API.
+// fare grafico con tooltip e rendere il tutto responsive
+ 
+function App() {
+    return (
+    <div>Hello World</div>);
+};
 
-
-const w = 500;
-const h = 100;
-
-
-const svg = d3.select("body")
-            .append("svg")
-            .attr("width", w)
-            .attr("height", h)
-
-svg.selectAll("rect")
-            .data(dataset)
-            .enter()
-            .append("rect")
-            .attr("width", )
+ReactDOM.render(<App />, document.getElementById("root"));
