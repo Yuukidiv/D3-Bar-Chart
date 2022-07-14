58fe2a5d5447597f7ddb86f787b7142ac5c95a23
Yuukidiv <67429515+Yuukidiv@users.noreply.github.com>
2022-07-14 18:40:29 +0200

    Broken code

--- app.js
+++ app.js
@@ -15,13 +15,15 @@ function App() {
     return (
     <div>
         <h1>Covid Stats</h1>
-    <BarChart 
-        data={countryData} 
-        width={countryData.length * 5} 
-        height={500} 
-        widthOfBar={5} 
-        dataType={"casesPerOneMillion"} 
-        />
+        <div className="visHolder">
+            <BarChart 
+            data={countryData} 
+            width={countryData.length * 5} 
+            height={500} 
+            widthOfBar={5} 
+            dataType={"casesPerOneMillion"} 
+            />
+        </div>
     </div>
         );
 };
@@ -32,8 +34,13 @@ function BarChart({data, height, width, widthOfBar, dataType}) {
     }, [data]);
 
     const createBarChart = () => {
-        const countryData = data.map(country => country["casesPerOneMillion"]);
-        const countries = data.map(country => country.country);
+        const countryData = data.map((country) => country["casesPerOneMillion"]);
+        const countries = data.map((country) => country.country);
+        let tooltip = d3.select("visHolder")
+                            .append("div")
+                            .attr("id", "tooltip")
+                            .style("opacity", 0);
+
         const dataMax = d3.max(countryData);
         const yScale = d3.scaleLinear()
                             .domain([0, dataMax])
@@ -42,14 +49,23 @@ function BarChart({data, height, width, widthOfBar, dataType}) {
             .selectAll("rect")
             .data(countryData)
             .enter()
-            .append("rect")
+            .append("rect");
+            d3.select("svg")
+            .selectAll("rect")
             .style("fill", (d, i)=> ( i % 2 == 0 ? "#b8f8f8" : "#b3f872"))
             .attr("x", (d, i) => i * widthOfBar)
             .attr("y", (d, i) => height - yScale(d + dataMax * 0.1))
             .attr("height", (d, i) => yScale(d + dataMax * 0.1))
-            .attr("width", widthOfBar);
+            .attr("width", widthOfBar)
+            .on("mouseover", (d, i) => {
+                tooltip.style("opacity", 0.9);
+                tooltip
+                .html(countries[i] + `<br/> ${dataType}: ` + d)
+                .style("left", i * widthOfBar + 20 + "px")
+                .style("top", d3.event.pageY - 170 + "px");
+            });
 
-    }   
+    }  ;
 
     return (
         <svg width={width} height={height}></svg>
